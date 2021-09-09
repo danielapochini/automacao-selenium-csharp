9e70340935f886b3f47106670122add48764be28
danielapochini <daniela.pochini@fatec.sp.gov.br>
2021-09-09 11:34:28 -0300

    Extent Report

--- Specflow/Hooks/GeneralHook.cs
+++ Specflow/Hooks/GeneralHook.cs
@@ -1,4 +1,7 @@
 ﻿using AutomacaoSeleniumCSharp.ComponentHelper;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using AventStack.ExtentReports.Reporter;
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -13,14 +16,97 @@ namespace AutomacaoSeleniumCSharp.Specflow.Hooks
     public class GeneralHook : XunitContextBase
     {
         private static ScenarioContext _scenarioContext;
-        private static FeatureContext _featureContext; 
+        private static FeatureContext _featureContext;
+        private static AventStack.ExtentReports.ExtentReports _extentReports;
+        private static ExtentHtmlReporter _extentHtmlReporter;
+        private static ExtentTest _feature;
+        private static ExtentTest _scenario;
 
         public GeneralHook(ITestOutputHelper output) : base(output)
         {
         }
 
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            _extentHtmlReporter = new ExtentHtmlReporter(@"C:\Data\log\");
+            _extentHtmlReporter.Config.ReportName = "testreport.html";
+            _extentHtmlReporter.Config.Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Dark;
+            _extentReports = new AventStack.ExtentReports.ExtentReports();
+            _extentReports.AttachReporter(_extentHtmlReporter);
+        }
+
+        [BeforeFeature]
+        public static void BeforeFeatureStart(FeatureContext featureContext)
+        {
+            if (null != featureContext)
+            {
+                _feature = _extentReports.CreateTest<Feature>(featureContext.FeatureInfo.Title, featureContext.FeatureInfo.Description);
+            }
+        }
+
+        [BeforeScenario]
+        public static void BeforeScenarioStart(ScenarioContext scenarioContext)
+        {
+            if (null != scenarioContext)
+            {
+                _scenarioContext = scenarioContext;
+                _scenario = _feature.CreateNode<Scenario>(scenarioContext.ScenarioInfo.Title, scenarioContext.ScenarioInfo.Description);
+            }
+        }
+
+        [AfterStep]
+        public void AfterEachStep()
+        {
+            // logic -> Given ,when or then
+            // add the node
+
+            ScenarioBlock scenarioBlock = _scenarioContext.CurrentScenarioBlock;
+
+            //switch case
+
+            switch (scenarioBlock)
+            {
+                case ScenarioBlock.Given:
+                    CreateNode<Given>();
+                    break;
+                case ScenarioBlock.When:
+                    CreateNode<When>();
+                    break;
+                case ScenarioBlock.Then:
+                    CreateNode<Then>();
+                    break;
+                default:
+                    CreateNode<And>();
+                    break;
+            }
+
+        }
+
+        public void CreateNode<T>() where T : IGherkinFormatterModel
+        {
+            if (_scenarioContext.TestError != null)
+            {
+                string name = @"C:\Data\log\" + _scenarioContext.ScenarioInfo.Title.Replace(" ", "") + ".jpeg";
+                GenericHelper.TakeScreenshot(name);
+                _scenario.CreateNode<T>(_scenarioContext.StepContext.StepInfo.Text).Fail(_scenarioContext.TestError.Message + "\n" + _scenarioContext.TestError.StackTrace)
+                    .AddScreenCaptureFromPath(name);
+            }
+            else
+            {
+                _scenario.CreateNode<T>(_scenarioContext.StepContext.StepInfo.Text).Pass("");
+            }
+        }
+
+        [AfterTestRun]
+        public static void AfterTestRun()
+        {
+            _extentReports.Flush();
+        }
+
+
         [BeforeFeature("Tag1")]
-        public static void BeforeFeature(FeatureContext featureContext)
+        public static void BeforeFeature()
         {
             Console.WriteLine("BeforeFeature Hook");
         }
