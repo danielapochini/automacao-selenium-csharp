89a1041b38a96dec801acb7ae37f88a1ab965aef
danielapochini <daniela.pochini@fatec.sp.gov.br>
2021-09-09 11:01:19 -0300

    Grids

---
+++ ComponentHelper/GridHelper.cs
@@ -0,0 +1,65 @@
+﻿using AutomacaoSeleniumCSharp.Settings;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomacaoSeleniumCSharp.ComponentHelper
+{
+    public class GridHelper
+    {
+        internal static string GetTableXpath(string locator, int row, int col)
+        {
+            return $"{locator}//tbody//tr[{row}]//td[{col}]";
+        }
+
+        private static IWebElement GetGridElement(string locator, int row, int col)
+        {
+            var xpath = GetTableXpath(locator, row, col);
+            if (GenericHelper.IsElementPresent(By.XPath(xpath + "//a")))
+            {
+                return ObjectRepository.Driver.FindElement(By.XPath(xpath + "//a"));
+            }
+            else if (GenericHelper.IsElementPresent(By.XPath(xpath + "//input")))
+            {
+                return ObjectRepository.Driver.FindElement(By.XPath(xpath + "//input"));
+            }
+            else
+            {
+                return ObjectRepository.Driver.FindElement(By.XPath(xpath));
+            }
+        }
+
+        public static string GetColumnValue(string @locator, int @row, int @col)
+        { 
+            return GetGridElement(locator, row, col).Text;
+        }
+
+        public static IList<string> GetAllValues(string @locator)
+        {
+            List<string> list = new List<string>();
+
+            var row = 1;
+            var col = 1;
+
+            while (GenericHelper.IsElementPresent(By.XPath(GetTableXpath(locator, row, col))))
+            {
+                while (GenericHelper.IsElementPresent(By.XPath(GetTableXpath(locator, row, col))))
+                {
+                    list.Add(ObjectRepository.Driver.FindElement(By.XPath(GetTableXpath(locator, row, col))).Text);
+                    col++;
+                }
+                row++;
+                col = 1;
+            }
+            return list;
+
+        }
+
+        public static void ClickButtonInGrid(string @locator, int @row, int @col)
+        {
+            GetGridElement(locator, row, col).Click();
+        }
+
+    }
+}
---
+++ TestScript/Grid/TestWebTable.cs
@@ -0,0 +1,19 @@
+﻿using AutomacaoSeleniumCSharp.BaseClasses;
+using AutomacaoSeleniumCSharp.ComponentHelper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AutomacaoSeleniumCSharp.TestScript.Grid
+{
+    public class TestWebTable : IClassFixture<BaseClass>
+    {
+        [Fact]
+        public void TestGrid()
+        {
+            NavigationHelper.NavigateToUrl("http://demos.telerik.com/kendo-ui/grid/custom-command");
+            GridHelper.ClickButtonInGrid("//div[@class='k-grid-content k-auto-scrollable']//table[@role='grid']", 2, 3);
+        }
+    }
+}
